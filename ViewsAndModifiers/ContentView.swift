39c5b4f596239ea760ad142ae194e38b67338f42
gv4383 <gregory.vargas18@gmail.com>
2021-11-02 15:37:46 -0500

    Finish project

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,10 +7,25 @@
 
 import SwiftUI
 
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+            .padding()
+    }
+}
+
+extension View {
+    func titleStyle () -> some View {
+        self.modifier(Title())
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
+            .titleStyle()
     }
 }
 
